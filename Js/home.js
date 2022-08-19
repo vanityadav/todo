9284acfccf0058645ac80c4b045798cb03f9a77d
vanityadav <110538930+vanityadav@users.noreply.github.com>
2022-08-19 11:42:48 +0530

    Sign Up Page Added

--- Js/home.js
+++ Js/home.js
@@ -39,22 +39,62 @@ const signup = document.querySelector('.signup-button')
 const signinModal = document.querySelector('.signin-modal')
 const signupModal = document.querySelector('.signup-modal')
 
+const exitbtn = Array.from(document.querySelectorAll('.exit'))
 
+signinModal.addEventListener('submit' , homepageHeader)
+signupModal.addEventListener('submit' , homepageHeader)
 
 signin.addEventListener('click', () => { signinModal.showModal() })
 signup.addEventListener('click', () => { signupModal.showModal() })
 
 
-// const exitbtn = document.querySelector('.exit')
-// exitbtn.addEventListener('click', () => { signinModal.close() })
+exitbtn.forEach(element => { element.addEventListener('click', closeModal )})
 
-const exitbtn = Array.from(document.querySelectorAll('.exit'))
-exitbtn.forEach(element => {
-  element.addEventListener('click', function (event) {
-    let buttonClicked = event.target
-    let buttonClass = buttonClicked.parentElement.parentElement.className
-    console.log(buttonClass)
-    buttonClass.close();
-  })
-});
+function closeModal(event) {
+    let buttonClicked = event.target.parentElement.parentElement
+    buttonClicked.close();
+}
+
+function homepageHeader() {
+  
+  document.getElementById('header').innerHTML=` <div class="header-menus logo">
+        <img
+          class="logosvg"
+          src="Images/todologo.svg"
+          alt="logo"
+          onclick="location.href='index.html'"
+          type="button"
+        />
+        <button class="btn-header">ToDo</button>
+      </div>
+      <div class="header-menus myday">
+        <img class="mydaylogo" src="Images/myday.svg" />
+        <button class="btn-header">My Day</button>
+      </div>
+      <div class="header-menus calender">
+        <img class="calenderlogo" src="Images/calander.svg" />
+        <button class="btn-header">Calender</button>
+      </div>
+      <div class="header-menus projects">
+        <img class="projectslogo" src="Images/projects.svg" />
+        <button class="btn-header">Projects</button>
+      </div>
+      <div class="empty"></div>
+      <div class="searchbar">
+        <input
+          class="searchbarinput"
+          type="text"
+          placeholder="Search everywhere......"
+        />
+        <img class="searchicon" src="Images/searchicon.svg" alt="search icon" />
+      </div>
+      <div class="username">
+        <p id="username">hi, Adam!</p>
+        <img class="account-image" src="Images/accountimage.svg" />
+        <div class="notificationsbox"></div>
+      </div>`
+ 
+}
+
+
 
