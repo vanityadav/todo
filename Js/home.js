e08093f33cefc9bf4aa3b9157dcc34dcd4386a6a
vanityadav <110538930+vanityadav@users.noreply.github.com>
2022-08-19 11:45:56 +0530

    Update home.js

--- Js/home.js
+++ Js/home.js
@@ -1,37 +1,3 @@
-// const buttons = Array.from(document.getElementsByClassName('btn-header'))
-// buttons.forEach(element => {element.addEventListener("click", getheader)});
-
-// function getheader() {
-//     const oldBody = document.getElementById('changebody')
-//     oldBody.remove()
-//     const mainbox = document.getElementById('mainbody')
-//     mainbox.innerHTML = `
-//     <div id="changebody">
-//         <div class="mainbox-left">
-//           <p class="homepagetext">
-//             ToDo gives you<br />
-//             focus from<br />
-//             work to play.
-//           </p>
-//           <p class="download-text">Available on</p>
-//           <div class="download-logos">
-//             <button class="download-app-button">
-//               <img src="Images/appstore.svg" />
-//             </button>
-//             <button class="download-app-button">
-//               <img src="Images/windowsstore.svg" />
-//             </button>
-//             <button class="download-app-button">
-//               <img src="Images/playstore.svg" />
-//             </button>
-//           </div>
-//         </div>
-      
-//       </div>
-//     `
-
-// }
-
 
 const signin = document.querySelector('.signin-button')
 const signup = document.querySelector('.signup-button')
